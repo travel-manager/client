bd2c07e6569c76034fdbff48a53257b5f47a1ef7
unknown <sk.roikonen@gmail.com>
2019-02-09 21:21:42 +0200

    api

--- server.js
+++ server.js
@@ -73,18 +73,9 @@ app.post("/api/travellers", function(req, res) {
  *    PUT: update traveller by id
  *    DELETE: deletes traveller by id
  */
+
 app.get("/api/travellers/:username", function(req, res) {
   db.collection(TRAVELLERS_COLLECTION).findOne({ username: new ObjectID(req.params.username) }, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to get traveller");
-    } else {
-      res.status(200).json(doc);
-    }
-  });
-});
-
-app.get("/api/travellers/:id", function(req, res) {
-  db.collection(TRAVELLERS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to get traveller");
     } else {
