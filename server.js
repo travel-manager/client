4950b12891b18d8b92c72f1109b4719e5cec342f
unknown <sk.roikonen@gmail.com>
2019-02-10 14:39:17 +0200

    fixing

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://sakari:m1ukuma
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+  var server = app.listen(process.env.PORT || 4200, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
